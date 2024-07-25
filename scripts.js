f23ee7f63562c2a50f36d546e44f4fc71748c497
brian-cairns <develop@prostrategix.com>
2024-07-25 11:46:38 -0400

    inital commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const refId= 10e16*Math.random();
+const url = "https://withpersona.com/verify?inquiry-template-id=itmpl_NxU5CMYGh7ZKFT5VU2XJfUh3beC4&environment-id=env_4M1FZXy9kptN1LXUMgvuTs9jxa42&referrence-id=" + refId;
+
+button = document.getElementById('fire');
+button.addEventListener('click', () => {
+console.log('Clicked and starting app...')
+fetch(`${url}`, {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({ refId})})
+})
+
